7623a718cb30a7841f9404ac81adfa6a3528fab8
Daniel Kantor <home@Kantors-MacBook-Pro.local>
2019-12-01 22:05:21 -0500

    Updated Front Page UI

---
+++ lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF212128);
+Color redColor = new Color(0xFFc72c41);
+
+TextStyle mainPageTitleStyle = new TextStyle( fontFamily: 'Avenier', 
+                                              fontWeight: FontWeight.bold,
+                                              color: darkGreyColor,
+                                              fontSize: 40,
+                                            );
